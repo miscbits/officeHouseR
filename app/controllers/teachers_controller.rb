ff0f1bf8dbe11a9887d62ed7a28c3b30516390e5
miscbits <willow6876@gmail.com>
2015-03-25 01:40:38 -0400

    upgraded to rails 4. added signup page for teachers. Only temporary

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -8,5 +8,24 @@ class TeachersController < ApplicationController
   	@teacher = Teacher.new
   end
 
+  def create
+    @teacher = Teacher.new(teacher_params)
+    if @teacher.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
 
+  def destroy
+  end
+
+  private
+
+    def teacher_params
+      params.require(:teacher).permit(:first_name, :last_name, :email, :password,
+                                   :password_confirmation)
+    end
+
+   
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -6,7 +6,7 @@ class Teacher < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, format: { with: //\A[\w+\-.]+@wcupa\.edu/ }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@wcupa\.edu/ }, uniqueness: { case_sensitive: false }
 
   has_secure_password
   validates :password_digest, length: { minimum: 6 }
