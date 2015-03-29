23ba15cc930adde7ee7ca24c6752ae3305060c0f
miscbits <willow6876@gmail.com>
2015-03-28 21:35:31 -0400

    removed redundant errors

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -5,7 +5,7 @@ class Teacher < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, format: { with: /\A[\w+\-.]+@wcupa\.edu/ }, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: /\A[\w+\-.]+@wcupa\.edu/ }, uniqueness: { case_sensitive: false }
 
   has_secure_password
   validates :password_digest, length: { minimum: 6 }
