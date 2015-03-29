6e163cedbd4419fd9234167cd65eb0643a1f1518
miscbits <willow6876@gmail.com>
2015-03-29 11:41:36 -0400

    removed references to nil classes

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -3,7 +3,7 @@ class AvailabilitiesController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
 	def new
-  	@availability = Availabilities.new
+  	@availability = Availability.new
   end
 
   def create
