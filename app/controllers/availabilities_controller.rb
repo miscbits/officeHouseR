12c39b168d4d320be5f8d4f4686ea238fe378631
miscbits <willow6876@gmail.com>
2015-03-31 16:41:01 -0400

    added destroy method for availabilities to fix exception error

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,6 +1,7 @@
 class AvailabilitiesController < ApplicationController
 
   before_action :logged_in_teacher, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
 	def new
   	@availability = current_teacher.availability.new teacher: @teacher
@@ -9,7 +10,7 @@ class AvailabilitiesController < ApplicationController
   def create
     @availability = current_teacher.availabilities.build(availability_params) 
     if @availability.save
-      flash[:success] = "Micropost created!"
+      flash[:success] = "Availability created!"
       redirect_to current_teacher
     else
       render 'static_pages/home'
@@ -17,11 +18,19 @@ class AvailabilitiesController < ApplicationController
   end
 
   def destroy
+    @availability.destroy
+    flash[:success] = "Availability deleted"
+    redirect_to current_teacher || root_url
   end
 
   private 
     def availability_params
       params.require(:availability).permit(:meeting_time, :meeting_location, :meeting_day)
+    end
+
+    def correct_user
+      @availability = current_teacher.availabilities.find_by(id: params[:id])
+      redirect_to root_url if @availability.nil?
     end
 
 end
